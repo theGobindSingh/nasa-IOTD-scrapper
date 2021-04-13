b151fa31003a5bd601a89d547e0a3eab4eb1fcfd
SinghGobind <58527382+SinghGobind@users.noreply.github.com>
2021-04-13 22:51:05 +0530

    Add files via upload

---
+++ les.py
@@ -0,0 +1,18 @@
+import os
+import requests #pip install requests
+import json
+
+loc = "D:/Nasa scrapper/New folder" #download location
+
+url_base="https://www.nasa.gov/api/2/ubernode/"
+
+uNum=[470022,470016,469987,469945,469909,469849,469816,469620,469738,469627,469619,469571,469478,469416,469211,469253,469179,469209,469178,469142]
+
+for x in uNum:
+    r = requests.get(url_base+str(x))
+    js=json.loads(r.text)
+    il= str (((js["_source"])["master-image"])["uri"])
+    il= il.replace("public://","https://www.nasa.gov/sites/default/files/")
+
+    with open(os.path.join(loc,str(x)+".png"),"wb") as f:
+        f.write(requests.get(il).content)
