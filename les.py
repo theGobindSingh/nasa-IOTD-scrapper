876b729c2743a26055f36ced592c432f5b55978e
SinghGobind <58527382+SinghGobind@users.noreply.github.com>
2021-04-14 15:46:55 +0530

    Update les.py

--- les.py
+++ les.py
@@ -12,7 +12,9 @@ except:
     print("")
 
 loc = docs+folder_name
+
 print ("save location: "+loc)
+
 url_base="https://www.nasa.gov/api/2/ubernode/"
 
 uNum=[470022,470016,469987,469945,469909,469849,469816,469620,469738,469627,469619,469571,469478,469416,469211,469253,469179,469209,469178,469142]
@@ -24,4 +26,5 @@ for x in uNum:
     il= il.replace("public://","https://www.nasa.gov/sites/default/files/")
 
     with open(os.path.join(loc,str(x)+".png"),"wb") as f:
-        f.write(requests.get(il).content)
+        f.write(requests.get(il).content) 
+    f.close()
